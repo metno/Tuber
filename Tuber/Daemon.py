c805816d693a5d85ea79bfbff4e22636c7b74f2c
Lars Jørgen Solberg <larsjs@met.no>
2016-07-12 11:24:51 +0000

    actually move messages from the entrypoint

--- Tuber/Daemon.py
+++ Tuber/Daemon.py
@@ -4,5 +4,30 @@
 
 import Tuber
 
+from argparse import ArgumentParser
+from urllib.parse import urlsplit
+
+def makeAdapter(url, direction):
+    split = urlsplit(url)
+
+    if split.scheme == 'gts':
+        return Tuber.TCPAdapter(direction, split.hostname, split.port)
+    else:
+        raise ArgumentError("Unsupported protocol {} in {}: ".format(split.scheme, url))
+
 def main():
-    print("Hello world")
+    parser = ArgumentParser()
+    parser.add_argument("source")
+    parser.add_argument("destination")
+    args = parser.parse_args()
+
+    while True:
+        try:
+            sender = makeAdapter(args.destination, 'output')
+            receiver = makeAdapter(args.source, 'input')
+
+            for msg in receiver:
+                sender.send(msg)
+
+        except ConnectionError as e:
+            sys.stderr.write(str(e))
